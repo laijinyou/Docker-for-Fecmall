32393ed8974604117b2e6162198a2c5980dbeb84
laijinyou <1183820@qq.com>
2022-07-04 11:22:14 +0900

    install tailwind css

---
+++ www/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./fecmall/appfront/theme/terry/theme01/**/*.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
