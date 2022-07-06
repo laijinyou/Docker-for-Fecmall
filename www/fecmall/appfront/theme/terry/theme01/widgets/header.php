a1a2878ee62c058dc8c891516df26f374a0c650a
laijinyou <1183820@qq.com>
2022-07-06 20:58:46 +0900

    Footer Header Main

--- www/fecmall/appfront/theme/terry/theme01/widgets/header.php
+++ www/fecmall/appfront/theme/terry/theme01/widgets/header.php
@@ -1,87 +1,47 @@
-<?php
-/**
- * FecShop file.
- *
- * @link http://www.fecshop.com/
- * @copyright Copyright (c) 2016 FecShop Software LLC
- * @license http://www.fecshop.com/license/
- */
-?>
-<div id="top_nav">
-	<input type="hidden" class="currentBaseUrl" value="<?= $currentBaseUrl ?>" />
-	<input type="hidden" class="logoutUrl" value="<?= $logoutUrl ?>" />
-	<input type="hidden" class="logoutStr" value="<?= Yii::$service->page->translate->__('Logout'); ?>" />
-	<input type="hidden" class="welcome_str" value="<?= Yii::$service->page->translate->__('Welcome!'); ?>" />
-	<div class="top_nav_inner">	    
-		<div class="top_nav_left">
-			<dl class="top_lang">
-				<dt><span class="current_lang" rel="<?= $currentStore ?>"><?= $currentStoreLang ?></span></dt>
-				<dd class="lang_list">
-					<ul>
-						<?php foreach($stores as $store=> $langName):   ?>
-							<li  class="store_lang"  rel="<?= $store ?>"><a href="javascript:void(0)"><?= $langName ?></a></li>
-						<?php endforeach; ?>
-						</ul>
-				</dd>
-			</dl>
-			<!-- 币种选择 -->
-			<dl class="top_currency">
-				<dt><span class="current_currency"><label><?= $currency['symbol'] ?></label><?= $currency['code'] ?></span></dt>
-				<dd class="currency_list">
-					<ul>
-					
-					<?php foreach($currencys as $c):    ?>
-						<li rel="<?= $c['code'] ?>"><label><?= $c['symbol'] ?></label><?= $c['code'] ?></li>
-					<?php endforeach; ?>							
-					</ul>
-				</dd>
-			</dl>
-		</div>
-		<div class="top_nav_right">
-			<div class="login-text t_r">
-				<span id="js_isNotLogin">
-					<a href="<?= Yii::$service->url->getUrl('customer/account/login') ?>" rel="nofollow"><?= Yii::$service->page->translate->__('Sign In / Join Free'); ?></a>
-				</span>
-			</div>
-			<dl class="top_account t_r">
-				<dt>
-					<a href="<?= Yii::$service->url->getUrl('customer/account') ?>" rel="nofollow" class="mycoount"></a>
-				</dt>
-				<dd style="">
-					<ul>
-						<li><a href="<?= Yii::$service->url->getUrl('customer/account') ?>" rel="nofollow"><?= Yii::$service->page->translate->__('My Account'); ?></a></li>
-						<li><a href="<?= Yii::$service->url->getUrl('customer/order') ?>" rel="nofollow"><?= Yii::$service->page->translate->__('My Orders'); ?></a></li>
-						<li><a href="<?= Yii::$service->url->getUrl('customer/productfavorite') ?>" rel="nofollow"><?= Yii::$service->page->translate->__('My Favorites'); ?></a></li>
-						<li><a href="<?= Yii::$service->url->getUrl('customer/productreview') ?>" rel="nofollow"><?= Yii::$service->page->translate->__('My Review'); ?></a></li>
-					</ul>
-				</dd>
-			</dl>
-			<div class="mywish t_r">
-				<a href="<?= Yii::$service->url->getUrl('customer/productfavorite') ?>">
-					<span class="mywishbg"></span>
-				</a>
-				<span class="mywish-text" id="js_favour_num">0</span>
-			</div>
-			<div class="mycart t_r">
-				<a href="<?= Yii::$service->url->getUrl('checkout/cart') ?>">
-					<span class="mycartbg" id="js_topBagWarp"></span>
-				</a>
-				<span class="mycart-text" id="js_cart_items">0</span>
-			</div>
-		</div>
-	</div><!--end .top_nav_inner-->
-</div><!--end #top_nav-->
-
-<div id="top_main">
-	<div class="top_main_inner pr">
-		<div class="top_header clearfix">
-			<div class="topSeachForm">
-				<?= Yii::$service->page->widget->render('base/topsearch',$this); ?>
-			</div>
-			<div class="logo"><a titel="fecshop logo" href="<?= $homeUrl ?>" style="">
-				<img src="<?= $logoImgUrl ?>"  />
-			</a></div>
-		</div><!--end .top_header-->
-    </div><!--end .top_main_inner-->
-</div>
-		
+<div class="h-32 bg-black">
+		<div class="max-w-screen-xl mx-auto">
+      <!-- Menu,Logo,Languages,account,shopping-cart-->
+      <div class="h-16 text-white flex justify-between">
+        <div class="p-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="px-2">
+          <div class="bg-zinc-700 h-16 pt-1 px-2 transform -skew-x-12">
+            <img class="transform skew-x-12" src="https://www.digikey.cn/-/media/Images/Header/logo_dk.png" alt="logo">
+            <div class="text-center text-xs italic font-bold transform skew-x-12">德州电子</div>
+          </div>
+        </div>
+        <div class="w-full py-4 pl-4">
+          <img class="w-8" src="https://www.digikey.com/-/media/Images/Global/Flags/Square/CN.png" alt="languages">
+        </div>
+        <!-- account -->
+        <div class="p-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+          </svg>
+        </div>
+        <!-- shopping-cart -->
+        <div class="p-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+        </div>
+      </div>
+      <!-- Search -->
+      <div class="h-16 bg-zinc-500">
+        <div class="flex justify-start py-2 mx-4">
+          <input 
+            type="search" 
+            class="w-full h-11 border-0 focus:ring-0 appearance-none"
+          >
+          <button class="bg-white border-0">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-11 text-red-500" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+	</div>
