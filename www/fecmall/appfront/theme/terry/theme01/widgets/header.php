4e9181b8a88825435101c1df55b239263a8381af
laijinyou <1183820@qq.com>
2022-07-06 23:17:16 +0900

    search

--- www/fecmall/appfront/theme/terry/theme01/widgets/header.php
+++ www/fecmall/appfront/theme/terry/theme01/widgets/header.php
@@ -1,47 +1,46 @@
 <div class="h-32 bg-black">
-		<div class="max-w-screen-xl mx-auto">
-      <!-- Menu,Logo,Languages,account,shopping-cart-->
-      <div class="h-16 text-white flex justify-between">
-        <div class="p-2">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
-          </svg>
-        </div>
-        <div class="px-2">
-          <div class="bg-zinc-700 h-16 pt-1 px-2 transform -skew-x-12">
-            <img class="transform skew-x-12" src="https://www.digikey.cn/-/media/Images/Header/logo_dk.png" alt="logo">
-            <div class="text-center text-xs italic font-bold transform skew-x-12">德州电子</div>
-          </div>
-        </div>
-        <div class="w-full py-4 pl-4">
-          <img class="w-8" src="https://www.digikey.com/-/media/Images/Global/Flags/Square/CN.png" alt="languages">
-        </div>
-        <!-- account -->
-        <div class="p-2">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-          </svg>
-        </div>
-        <!-- shopping-cart -->
-        <div class="p-2">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
+  <div class="max-w-screen-xl mx-auto">
+    <div class="h-16 text-white flex justify-between">
+      <!-- menu -->
+      <div class="p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+        </svg>
+      </div>
+      <!-- logo -->
+      <div class="px-2">
+        <div class="bg-zinc-700 h-16 pt-1 px-2 transform -skew-x-12">
+          <img class="transform skew-x-12" src="https://www.digikey.cn/-/media/Images/Header/logo_dk.png" alt="logo">
+          <div class="text-center text-xs italic font-bold transform skew-x-12">德州电子</div>
         </div>
       </div>
-      <!-- Search -->
-      <div class="h-16 bg-zinc-500">
-        <div class="flex justify-start py-2 mx-4">
-          <input 
-            type="search" 
-            class="w-full h-11 border-0 focus:ring-0 appearance-none"
-          >
-          <button class="bg-white border-0">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-11 text-red-500" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-            </svg>
-          </button>
-        </div>
+      <!-- languages -->
+      <div class="w-full py-4 pl-4">
+        <img class="w-8" src="https://www.digikey.com/-/media/Images/Global/Flags/Square/CN.png" alt="languages">
+      </div>
+      <!-- account -->
+      <div class="p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+        </svg>
+      </div>
+      <!-- shopping-cart -->
+      <div class="p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+        </svg>
       </div>
     </div>
-	</div>
+    <!-- Search -->
+    <div class="h-16 bg-zinc-500">
+      <div class="flex justify-start py-2 mx-4">
+        <input type="search" class="w-full h-11 border-0 focus:ring-0 appearance-none">
+        <button class="bg-white border-0">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-11 text-red-500" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
