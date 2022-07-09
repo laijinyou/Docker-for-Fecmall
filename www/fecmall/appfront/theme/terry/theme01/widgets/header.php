1f31ade1749fc56d71936e22ed7d2b6859ef06a5
laijinyou <1183820@qq.com>
2022-07-09 19:50:25 +0900

    detele .gitinore file

--- www/fecmall/appfront/theme/terry/theme01/widgets/header.php
+++ www/fecmall/appfront/theme/terry/theme01/widgets/header.php
@@ -47,21 +47,91 @@
       </div>
 
 
-      <div class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full h-full opacity-60 bg-black">
+      <div class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full h-full bg-opacity-60 bg-black">
         <div class="relative w-3/4 h-full bg-white">
-          <div class="h-16 bg-black flex justify-between">
-            <img src="https://www.digikey.cn/-/media/Images/Header/logo_dk.png" alt="logo">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <div class="h-16 bg-black flex justify-between p-2.5">
+            <img class="h-11" src="https://www.digikey.cn/-/media/Images/Header/logo_dk.png" alt="logo">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </div>
-          <ul>
-            <li class="h-11 border-b-2">产品</li>
-            <li class="h-11 border-b-2">制造商</li>
-            <li class="h-11 border-b-2">资源</li>
+          <ul class="text-black hidden" id="mobile-menu-first">
+            <li class="h-11 border-b-2 flex justify-between">
+              <div class="my-3 ml-3">产品</div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-3 mr-3 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+              </svg>
+            </li>
+            <li class="h-11 border-b-2 flex justify-between">
+              <div class="my-3 ml-3">制造商</div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-3 mr-3 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+              </svg>
+            </li>
+            <li class="h-11 border-b-2 flex justify-between">
+              <div class="my-3 ml-3">资源</div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-3 mr-3 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+              </svg>
+            </li>
           </ul>
+
+          <div class="text-black" id="mobile-menu-second">
+            <div class="flex justify-start h-11 border-b-2" id="moblie-menu-back">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-600 my-2.5 ml-2.5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+              </svg>
+              <div class="text-xs my-3.5">后退</div>
+            </div>
+            <ul class="">
+              <li class="h-8 flex justify-between">
+                <div class="my-3.5 ml-3.5 text-sm">产品</div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-3 mr-3 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                </svg>
+              </li>
+              <li class="h-8 flex justify-between">
+                <div class="my-3.5 ml-3.5 text-sm">产品</div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-3 mr-3 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                </svg>
+              </li>
+              <li class="h-8 flex justify-between">
+                <div class="my-3.5 ml-3.5 text-sm">产品</div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-3 mr-3 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                </svg>
+              </li>
+            </ul>
+            <ul class="text-sm hidden">
+              <li class="h-8 py-3.5 pl-3.5">
+                产品
+              </li>
+              <li class="h-8 py-3.5 pl-3.5">
+                产品
+              </li>
+              <li class="h-8 py-3.5 pl-3.5">
+                产品
+              </li>
+              <li class="h-8 py-3.5 pl-3.5">
+                产品
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
+
+<script>
+  <?php $this->beginBlock('mobile_sider_menu') ?>
+  $(document).ready(function() {
+    $("#moblie-menu-back").click(function() {
+      $("#mobile-menu-first").removeClass("hidden");
+      $("#mobile-menu-second").addClass("hidden");
+    });
+  });
+  <?php $this->endBlock(); ?>
+</script>
+<?php $this->registerJs($this->blocks['mobile_sider_menu'], \yii\web\View::POS_END); ?>
