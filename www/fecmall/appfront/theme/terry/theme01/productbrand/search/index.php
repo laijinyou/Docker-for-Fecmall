71e4e55dfebff433dc61eb6fd28d2ba0d02d5b20
laijinyou <1183820@qq.com>
2022-07-13 18:02:26 +0900

    product brand search

--- www/fecmall/appfront/theme/terry/theme01/productbrand/search/index.php
+++ www/fecmall/appfront/theme/terry/theme01/productbrand/search/index.php
@@ -1,1 +1,317 @@
-<?= $name  ?>
+<div class="mt-16 bg-zinc-200">
+    <div>
+        <div class="p-4">
+            <h2 class="text-sm text-gray-500">制造商名册</h2>
+        </div>
+    </div>
+    <div class="max-w-screen-xl mx-auto">
+        <div class="bg-white m-4">
+            <div class="p-4">
+                制造商
+            </div>
+        </div>
+        <div class="m-4 lg:flex lg:justify-between">
+            <div class="bg-white p-4 w-full lg:w-96 h-max">
+                <div class="font-bold my-2">细化搜索</div>
+                <input type="text" class="w-full placeholder:text-xs" placeholder="搜索制造商">
+                <div class="my-4">
+                    <div class="border-2">
+                        <div class="flex justify-between py-3 px-2">
+                            <div>状态</div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-auto" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="border-x-2 border-b-2 p-2">
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">新制造商</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="border-2">
+                        <div class="flex justify-between py-3 px-2">
+                            <div>按照产品类型列出制造商</div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-auto" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="border-x-2 border-b-2 p-2">
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700 " />
+                            <span class="text-xs">机电 (160)</span>
+                        </div>
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">互连器件与导线 (324)</span>
+                        </div>
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">光电器件 (249)</span>
+                        </div>
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">无源器件 (111)</span>
+                        </div>
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">无源器件 (111)</span>
+                        </div>
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">无源器件 (111)</span>
+                        </div>
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">无源器件 (111)</span>
+                        </div>
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">无源器件 (111)</span>
+                        </div>
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">无源器件 (111)</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-indigo-700 text-sm font-bold my-8 lg:my-0 w-full lg:ml-8">
+                <h2 class="mb-4 text-xl text-black">制作商</h2>
+                <div class="lg:flex lg:justify-end lg:gap-2">
+                    <div>新制造商</div>
+                    <div>制造商清单</div>
+                    <div>打印 Excel 表格</div>
+                </div>
+                <div class="mt-4">
+                    <div class="mb-6 text-black">
+                        #
+                    </div>
+                    <div class="sm:grid sm:grid-cols-2">
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International111</a></a>
+                        </div>
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International2222</a></a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International333</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International44</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International555</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <div class="mb-6 text-black">
+                        A
+                    </div>
+                    <div class="sm:grid sm:grid-cols-2">
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International</a></a>
+                        </div>
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International</a></a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <div class="mb-6 text-black">
+                        B
+                    </div>
+                    <div class="sm:grid sm:grid-cols-2">
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International</a></a>
+                        </div>
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International</a></a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <div class="mb-6 text-black">
+                        C
+                    </div>
+                    <div class="sm:grid sm:grid-cols-2">
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International</a></a>
+                        </div>
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International</a></a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <div class="mb-6 text-black">
+                        D
+                    </div>
+                    <div class="sm:grid sm:grid-cols-2">
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International</a></a>
+                        </div>
+                        <div>
+                            <a href=""><a href="">Accelerated / Digi International</a></a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                        <div>
+                            <a href="">Accelerated / Digi International</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
