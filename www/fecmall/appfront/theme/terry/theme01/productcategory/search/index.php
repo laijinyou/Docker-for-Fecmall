acf0dcd3ba7707c06a48feb0091f4e9690a8ac52
laijinyou <1183820@qq.com>
2022-07-14 11:55:21 +0900

    category

--- www/fecmall/appfront/theme/terry/theme01/productcategory/search/index.php
+++ www/fecmall/appfront/theme/terry/theme01/productcategory/search/index.php
@@ -1,1 +1,52 @@
-<?= $name  ?>
+<div class="mt-16 bg-zinc-200">
+    <div class="max-w-screen-xl mx-auto">
+        <div class="">
+            <h2 class="text-2xl">产品索引</h2>
+        </div>
+        <div class="lg:flex lg:justify-between">
+            <div class="lg:w-96 h-max">
+                过滤
+            </div>
+            <div>
+                <div>
+                    <h3 class="font-bold border-b border-neutral-400">RF/IF,射频/中频和 RFID</h3>
+                    <div class="text-xs text-neutral-500">
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                    </div>
+                </div>
+                <div>
+                    <h3>RF/IF,射频/中频和 RFID</h3>
+                    <div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
