28bb355ae72b416fa1a20578d44da48bf50f29ad
laijinyou <1183820@qq.com>
2022-07-14 22:34:27 +0900

    productcategory

--- www/fecmall/appfront/theme/terry/theme01/productcategory/search/index.php
+++ www/fecmall/appfront/theme/terry/theme01/productcategory/search/index.php
@@ -1,49 +1,236 @@
-<div class="mt-16 bg-zinc-200">
-    <div class="max-w-screen-xl mx-auto">
-        <div class="">
+<div class="mt-16 sm:mt-0 bg-zinc-200">
+    <div class="max-w-screen-xl mx-auto py-4">
+        <div class="pb-2">
             <h2 class="text-2xl">产品索引</h2>
         </div>
-        <div class="lg:flex lg:justify-between">
-            <div class="lg:w-96 h-max">
-                过滤
-            </div>
-            <div>
-                <div>
-                    <h3 class="font-bold border-b border-neutral-400">RF/IF,射频/中频和 RFID</h3>
-                    <div class="text-xs text-neutral-500">
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+        <div class="lg:flex lg:justify-start gap-10">
+            <div class="lg:w-80 h-max bg-white p-4">
+                <div class="">
+                    <div class="border-b p-2 font-bold text-sm">
+                        过滤
+                    </div>
+                    <form class="p-2">
+                        <div class="relative w-full">
+                            <input type="search" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-red-500 focus:border-red-500 placeholder:text-xs" placeholder="结果再搜索" required="">
+                            <button type="submit" class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-red-700 rounded-r-lg border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
+                        </div>
+                    </form>
+                    <div class="p-2">
+                        <div>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">现货</span>
+                            <input type="checkbox" class="form-checkbox rounded text-red-700" />
+                            <span class="text-xs">符合RoHS</span>
+                        </div>
+                        <div>
+                            <span class="text-xs text-blue-700">+ 更多筛选条件</span>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="mt-4">
+                    <div class="border-b p-2 font-bold text-sm">
+                        类别
+                    </div>
+                    <div class="flex flex-col gap-2 text-xs p-2">
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">五金件，紧固件，配件</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">传感器，变送器</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">光学器件</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">光学检测设备</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">光电器件</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">分立半导体产品</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">创客/DIY，教育</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">原型开发，制造品</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">变压器</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);">RF/IF，射频/中频和 RFID</a>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <h3>RF/IF,射频/中频和 RFID</h3>
-                    <div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
-                        <div>RF 其它 IC 和模块 2,470种货品 </div>
+            </div>
+            <div class="flex flex-col gap-8 w-full">
+                <div class="">
+                    <h3 class="font-bold border-b border-neutral-400 pb-2">RF/IF,射频/中频和 RFID</h3>
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 text-xs text-neutral-500 mt-4">
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                    </div>
+                </div>
+                <div class="">
+                    <h3 class="font-bold border-b border-neutral-400 pb-2">RF/IF,射频/中频和 RFID</h3>
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 text-xs text-neutral-500 mt-4">
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                    </div>
+                </div>
+                <div class="">
+                    <h3 class="font-bold border-b border-neutral-400 pb-2">RF/IF,射频/中频和 RFID</h3>
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 text-xs text-neutral-500 mt-4">
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                    </div>
+                </div>
+                <div class="">
+                    <h3 class="font-bold border-b border-neutral-400 pb-2">RF/IF,射频/中频和 RFID</h3>
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 text-xs text-neutral-500 mt-4">
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                    </div>
+                </div>
+                <div class="">
+                    <h3 class="font-bold border-b border-neutral-400 pb-2">RF/IF,射频/中频和 RFID</h3>
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 text-xs text-neutral-500 mt-4">
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                    </div>
+                </div>
+                <div class="">
+                    <h3 class="font-bold border-b border-neutral-400 pb-2">RF/IF,射频/中频和 RFID</h3>
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 text-xs text-neutral-500 mt-4">
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
+                        <div class="col-span-1">RF 其它 IC 和模块 2,470种货品 </div>
                     </div>
                 </div>
             </div>
